cbcfd969d679e5c351bcdee5cd6edaabde2ee948
LeoBP1987 <leonardobp1987@gmail.com>
2025-02-14 15:06:28 -0300

    Conexão com o Postgres e criação dos modelos Posts, Acesso e UTM com seus respectivos paineis admin

--- webhook/admin.py
+++ webhook/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from webhook.models import Posts, Acessos, UTM
 
-# Register your models here.
+class PostsAdmin(admin.ModelAdmin):
+    list_display= ('id_post', )
+    list_display_links= ('id_post', )
+    list_per_page = 10
+    list_filter = ('id_post', )
+    search_fields = ('id_post', )
+    ordering = ('id_post', )
+
+admin.site.register(Posts, PostsAdmin)
+
+class AcessosAdmin(admin.ModelAdmin):
+    list_display= ('leitor', 'id_post', 'abertura')
+    list_display_links= ('leitor', 'id_post', 'abertura')
+    list_per_page = 10
+    list_filter = ('leitor__email', 'id_post__id_post', 'abertura')
+    search_fields = ('leitor__email', 'id_post__id_post', 'abertura')
+    ordering = ('leitor', 'id_post', 'abertura')
+
+admin.site.register(Acessos, AcessosAdmin)
+
+class UTMAdmin(admin.ModelAdmin):
+    list_display=('utm_source', 'utm_medium', 'utm_campaign', 'utm_channel', 'utm_variaveis')
+    list_display_links=('utm_source', 'utm_medium')
+    list_per_page = 10
+    list_filter = ('utm_source', 'utm_medium', 'utm_campaign', 'utm_channel')
+    search_fields = ('utm_source', 'utm_medium', 'utm_campaign', 'utm_channel')
+    ordering = ('utm_source', )
+
+admin.site.register(UTM, UTMAdmin)
--- webhook/models.py
+++ webhook/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.core.validators import EmailValidator
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Posts(models.Model):
+    id_post = models.IntegerField(db_index=True, primary_key=True)
+
+    def __str__(self):
+        return self.id_post
+
+class Acessos(models.Model):
+    leitor = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='acessos'
+    )
+    id_post = models.ForeignKey(
+        to=Posts,
+        on_delete=models.CASCADE,
+        related_name='acessos'
+    )
+    abertura = models.DateTimeField(db_index=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['leitor', 'abertura']),
+            models.Index(fields=['id_post', 'abertura'])
+        ]
+    
+
+    def __str__(self):
+        return f'Acesso de {self.email.email} a {self.id_post.id_post} em {self.abertura}.'
+    
+class UTM(models.Model):
+    acesso = models.ForeignKey(
+        to=Acessos,
+        on_delete=models.CASCADE,
+        related_name='utms'
+    )
+    utm_source = models.CharField(max_length=50, db_index=True)
+    utm_medium = models.CharField(max_length=50, db_index=True)
+    utm_campaign = models.CharField(max_length=50, db_index=True)
+    utm_channel = models.CharField(max_length=50, db_index=True)
+    utm_variaveis = models.JSONField() 
+
+    def __str__(self):
+        return f'UTM de {self.acesso.leitor.email} no post {self.acesso.id_post.id_post} em {self.acesso.abertura}'
