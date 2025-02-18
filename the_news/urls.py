7cf636d5b824f3404925b1711ae1f20e8710f9ff
LeoBP1987 <leonardobp1987@gmail.com>
2025-02-17 21:24:24 -0300

    Redirecionando rota raiz para o Webhook

--- the_news/urls.py
+++ the_news/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.permissions import AllowAny
+from webhook.views import WebhookViewSet
 from django.views.generic import RedirectView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -19,7 +20,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/webhook/', permanent=True)),
+    path('', WebhookViewSet.as_view(), name='Webhook-Raiz'),
     path('admin/', admin.site.urls),
     path('', include('webhook.urls')),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
